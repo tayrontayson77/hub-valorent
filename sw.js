cc28ebbf373ae23523759983d9ef8d79b5b86bb2
tayrontayson77 <tayron.tayson77@gmail.com>
2026-08-17 04:12:36 +0200

    feat: add VALOHUB offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='valohub-v1';
+const CORE=['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./'))))});
